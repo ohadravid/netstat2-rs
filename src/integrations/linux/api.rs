9d03aade3d5fe7ffb85309d181ff88e00f57d82c
Harry Cheng <chengyuhui1@gmail.com>
2020-11-18 23:51:07 +0800

    Add `iterate_sockets_info_without_pids` to linux

--- src/integrations/linux/api.rs
+++ src/integrations/linux/api.rs
@@ -9,6 +9,17 @@ pub fn iterate_sockets_info(
     af_flags: AddressFamilyFlags,
     proto_flags: ProtocolFlags,
 ) -> Result<impl Iterator<Item = Result<SocketInfo, Error>>, Error> {
+    Ok(attach_pids(iterate_sockets_info_without_pids(
+        af_flags,
+        proto_flags,
+    )?))
+}
+
+/// Iterate through sockets information without attaching PID.
+pub fn iterate_sockets_info_without_pids(
+    af_flags: AddressFamilyFlags,
+    proto_flags: ProtocolFlags,
+) -> Result<impl Iterator<Item = Result<SocketInfo, Error>>, Error> {
     let ipv4 = af_flags.contains(AddressFamilyFlags::IPV4);
     let ipv6 = af_flags.contains(AddressFamilyFlags::IPV6);
     let tcp = proto_flags.contains(ProtocolFlags::TCP);
@@ -32,7 +43,7 @@ pub fn iterate_sockets_info(
             }
         }
     }
-    Ok(attach_pids(iterators.into_iter().flatten()))
+    Ok(iterators.into_iter().flatten())
 }
 
 fn attach_pids(
