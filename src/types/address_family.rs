8b8bf3ff830a19fa48ef37ea0ebe27ec8c3c830e
Ohad Ravid <oravid@trigoretail.com>
2024-11-21 12:12:04 +0200

    Bump bitflags version

--- src/types/address_family.rs
+++ src/types/address_family.rs
@@ -1,5 +1,6 @@
 bitflags! {
     /// Set of address families.
+    #[derive(Debug, Clone, Copy, PartialEq)]
     pub struct AddressFamilyFlags: u8 {
         const IPV4 = 0b00000001;
         const IPV6 = 0b00000010;
--- src/types/protocol.rs
+++ src/types/protocol.rs
@@ -1,5 +1,6 @@
 bitflags! {
     /// Set of protocols.
+    #[derive(Debug, Clone, Copy, PartialEq)]
     pub struct ProtocolFlags: u8 {
         const TCP = 0b00000001;
         const UDP = 0b00000010;
