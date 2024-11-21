32eb3d6b70796afad8e13b3e0e6a0bacd3136f2a
Ohad Ravid <oravid@trigoretail.com>
2024-11-21 12:06:46 +0200

    Switch the bench to use criterion

--- benches/get_sockets_info_benchmark.rs
+++ benches/get_sockets_info_benchmark.rs
@@ -1,17 +1,13 @@
-#![feature(test)]
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use netstat2::*;
 
-extern crate test;
-
-#[cfg(test)]
-mod tests {
-    use netstat2::*;
-
-    #[bench]
-    fn bench_new(b: &mut test::Bencher) {
-        b.iter(|| {
-            let af_flags = AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6;
-            let proto_flags = ProtocolFlags::TCP | ProtocolFlags::UDP;
-            get_sockets_info(af_flags, proto_flags).unwrap();
-        });
-    }
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("get_sockets_info", |b| b.iter(|| {
+        let af_flags = AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6;
+        let proto_flags = ProtocolFlags::TCP | ProtocolFlags::UDP;
+        let _result = get_sockets_info(black_box(af_flags), proto_flags).unwrap();
+    }));
 }
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
